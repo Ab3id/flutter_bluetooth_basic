6af63fde82e882c34fab105f2dc898a100208307
Abeid Juma <abeidtwo@gmail.com>
2025-05-28 03:54:32 +0300

    remove registerWith method

--- android/src/main/java/com/tablemi/flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.java
+++ android/src/main/java/com/tablemi/flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.java
@@ -59,11 +59,11 @@ public class FlutterBluetoothBasicPlugin implements FlutterPlugin, MethodCallHan
 
   // plugin should still contain the static registerWith() method to remain compatible with apps
   // that don’t use the v2 Android embedding.
-  public static void registerWith(Registrar registrar) {
-    FlutterBluetoothBasicPlugin instance = new FlutterBluetoothBasicPlugin();
-    instance.createChannel(registrar.messenger());
-    registrar.addRequestPermissionsResultListener(instance);
-  }
+  // public static void registerWith(Registrar registrar) {
+  //   FlutterBluetoothBasicPlugin instance = new FlutterBluetoothBasicPlugin();
+  //   instance.createChannel(registrar.messenger());
+  //   registrar.addRequestPermissionsResultListener(instance);
+  // }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
